fa6de925779a6fb2198a152d2e6f426cf692c0bc
Student6 <Student6@10.8.33.157>
2017-05-05 12:29:22 -0400

    wahhh

---
+++ Symposium/src/project/Main.java
@@ -0,0 +1,17 @@
+package project;
+
+import javax.swing.*;
+
+public class Main {
+	
+	public void main(String[] args){
+			JFrame f = new JFrame("Hotello");
+			JButton b=new JButton("Click Here");  
+			b.setBounds(50,100,95,30);  
+			f.add(b);  
+			f.setSize(400,400);  
+			f.setLayout(null);  
+			f.setVisible(true);   
+	
+	}
+}
