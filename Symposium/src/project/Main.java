1f82c198f27d3a3fac2d847690afe46e09470d30
Student6 <Student6@10.8.33.157>
2017-05-12 13:13:08 -0400

    ye

--- Symposium/src/project/Main.java
+++ Symposium/src/project/Main.java
@@ -9,12 +9,23 @@ import javax.swing.*;
  
 
 public class Main {
+	
+	private static int iy = 600;
 		
-	int clickedF = 0;
-	static int buttonint = 2;
-	static String buttonidx = "1";
-	ArrayList<Integer> buttidx = new ArrayList<Integer>(); 
-	ArrayList<JButton> barr = new ArrayList<JButton>();
+	private static int clickedF;
+	private static int buttonint = 2;
+	private static String buttonidx = "2";
+	
+	//to upgrade buttons
+	private ArrayList<Integer> buttidx = new ArrayList<Integer>(); 
+	private ArrayList<JButton> barr = new ArrayList<JButton>();
+	
+	
+	//adding floors
+	private static int pathsi = 0;
+	private static String[] paths = {"blue.jpg","Haunted.png","lounge.jpg",
+			"piano.png","red.png","Room.png","rustic.png"};
+	
 	
 	//write method that will getText of button to see index (upgrade method)
 	
@@ -24,33 +35,45 @@ public class Main {
 	//Add button to barr
 	
 	public static void main(String[] args) {  
-		JFrame f=new JFrame();//creating instance of JFrame 
-		ImageIcon i = new ImageIcon("resources/blue.jpg");
-		JButton b=new JButton("pie",i);//creating instance of JButton  
-		buttonidx =Integer.toString(buttonint);
-		System.out.println(buttonidx);
+		JFrame f=new JFrame("Hotello");//creating instance of JFrame 
+		f.setSize(1200,1000);//400 width and 500 height  
+		
+		JPanel game = new JPanel();
+		game.setBounds(0,0,400,600);
+		game.setBackground(Color.blue);
+		
+		System.out.println("panel created");
+		
+		ImageIcon i = new ImageIcon("resources/Lobby.png");
+		JButton b=new JButton(buttonidx,i);//creating instance of JButton  
 		b.setBackground(Color.black);
 		b.setOpaque(true);
-		b.setBounds(130,100,300, 200);//x axis, y axis, width, height  
+		b.setBounds(500,100,300, 200);//x axis, y axis, width, height  
 		b.addActionListener(new ActionListener(){  
 			  public void actionPerformed(ActionEvent e){  
-			               
+			        game.add(makeFloor(game));
+			        
 			          }  
 			      });  
 	
 		
-		f.setSize(600,800);//400 width and 500 height  
 		
-		JScrollBar s=new JScrollBar();  
-		s.setBounds(f.getWidth()-50,100, 50,100);
+		JScrollPane s=new JScrollPane(game); 
 		
-		Room r = new Room(0,200,100,300,new ImageIcon("cherry.jpg"));
-		f.add(r);
 		
-		f.add(s);
-		f.add(b);//adding button in JFrame  
-		f.setLayout(null);//using no layout managers  
+		game.setAutoscrolls(true);
+		
+		game.add(b);//adding button in JFrame 
+		
+		System.out.println("button added");
+		f.add(game);
+		
+		System.out.println("panel added");
+		
+	
 		f.setVisible(true);//making the frame visible  
+		
+		System.out.println("frame visible");
 		
 		
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -58,6 +81,32 @@ public class Main {
 	
 	public Room makeRoom(){
 		return null;
+		
+	}
+	
+	public static JButton makeFloor(JPanel p){
+		ImageIcon icon = new ImageIcon("resources/" + paths[pathsi]);
+		JButton b = new JButton(buttonidx,icon);
+		b.setBounds(500,iy,400,200);
+		b.setBackground(Color.black);
+		b.setOpaque(true);
+		System.out.println(buttonidx);
+		b.addActionListener(new ActionListener(){  
+			  public void actionPerformed(ActionEvent e){  
+				  upgradeFloor();
+			          }  
+			      });  
+		p.repaint();
+		buttonint++;
+		buttonidx = Integer.toString(buttonint);
+		iy -= 150;
+		pathsi++;
+		return b;
+		
+	}
+	
+	public static void upgradeFloor(){
+		
 		
 	}
 }
